340e90a891ecf1aee748fe028ae4443e4c7d0eab
Aditya Oli <adityaoligehu@gmail.com>
2021-03-26 09:00:06 +1300

    Create get_all_non_running_pod_stats.sh

---
+++ get_all_non_running_pod_stats.sh
@@ -0,0 +1,15 @@
+NAMESPACES=$(kubectl get namespaces | grep -v NAME | awk '{print $1}')
+ALL_NAMESPACES=($NAMESPACES)
+for namespace in "${ALL_NAMESPACES[@]}"; do 
+  KUBENS_RUN=$(kubectl config set-context --current --namespace=$namespace)
+  echo "========================================= $namespace ========================================="  
+  all_namespace_pods=$(kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+  arrayOfPods=($all_namespace_pods)
+  for eachPod in "${arrayOfPods[@]}"; do 
+    podStatus=$(kubectl describe pods $eachPod | grep ^Status: | head -1 | awk '{print $2}' | tr -d '\n')
+      if [ "$podStatus" != "Running" ] && [ "$podStatus" != "Completed" ] && [ "$podStatus" != "Succeeded" ];
+        then
+          echo "$eachPod | $podStatus"
+      fi  
+  done  
+done
